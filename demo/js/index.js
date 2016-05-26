75713776d2c9ff227006645d14603966b2db40d4
Emmanouil Konstantinidis <manos@dabapps.com>
2016-05-26 14:41:12 +0100

    Prepare demo

---
+++ demo/js/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import LiveAPIEndpoints from '../../src/index';
+// var React = require('react');
+// var LiveAPIEndpoints = require('../../src');
+
+alert('asds')
+
+ReactDOM.render(
+  <h1>YEY</h1>, document.getElementById('demo')
+  // <LiveAPIEndpoints endpoint={data} />, document.getElementById('demo')
+);
+
+// var input = 'const getMessage = () => "Hello World";';
+// var output = Babel.transform(input, { presets: ['es2015'] }).code;
